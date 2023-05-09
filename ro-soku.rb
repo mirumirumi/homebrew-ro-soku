788adbbcbbd7cb42b553fb7a08235a5582aafc6b
mirumi <mail@mirumi.tech>
2023-05-09 22:48:40 +0900

    ci(release): do not use dedicated aarch64 for mac build

--- ro-soku.rb
+++ ro-soku.rb
@@ -1,25 +1,13 @@
 class Rosoku < Formula
   desc "Retrieve OHLCV ro-soku (means a candle in Japanese) from any exchange🕯️"
   homepage "https://github.com/mirumirumi/ro-soku"
+  url "https://github.com/mirumirumi/ro-soku/releases/download/0.1.0/ro-soku_x86_64_mac"
+  sha256 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   version "0.1.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/mirumirumi/ro-soku/releases/download/0.1.0/ro-soku_x86_64_mac"
-      sha256 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    elsif Hardware::CPU.arm?
-        url "https://github.com/mirumirumi/ro-soku/releases/download/0.1.0/ro-soku_aarch64_mac"
-      sha256 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    end
-  end
-
   def install
-    if Hardware::CPU.intel?
-      bin.install "ro-soku_x86_64_mac" => "ro-soku"
-    elsif Hardware::CPU.arm?
-      bin.install "ro-soku_aarch64_mac" => "ro-soku"
-    end
+    bin.install "ro-soku_x86_64_mac" => "ro-soku"
   end
 
   test do
