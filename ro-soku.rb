c732b1c2c87e0c260faf2d8fca847a8f5b9f30cd
mirumi <mail@mirumi.tech>
2023-05-10 00:47:34 +0900

    fix: formula class name

--- ro-soku.rb
+++ ro-soku.rb
@@ -1,4 +1,4 @@
-class Rosoku < Formula
+class RoSoku < Formula
   desc "Retrieve OHLCV ro-soku (means a candle in Japanese) from any exchange🕯️"
   homepage "https://github.com/mirumirumi/ro-soku"
   url "https://github.com/mirumirumi/ro-soku/releases/download/0.1.0/ro-soku_x86_64_mac"
