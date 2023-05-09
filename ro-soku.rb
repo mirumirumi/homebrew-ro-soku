1e2c04c5c08099a372555ccbcbe7813612ac18c6
mirumi <mail@mirumi.tech>
2023-05-10 00:14:25 +0900

    chore: fix `sed` indent

--- ro-soku.rb
+++ ro-soku.rb
@@ -2,7 +2,7 @@ class Rosoku < Formula
   desc "Retrieve OHLCV ro-soku (means a candle in Japanese) from any exchange🕯️"
   homepage "https://github.com/mirumirumi/ro-soku"
   url "https://github.com/mirumirumi/ro-soku/releases/download/0.1.0/ro-soku_x86_64_mac"
-      sha256 "91e7f4aa5b254ed4c58e511b67713358fbd2337f08168c5ec721a2e7ebff043f"
+  sha256 "91e7f4aa5b254ed4c58e511b67713358fbd2337f08168c5ec721a2e7ebff043f"
   version "0.1.0"
   license "MIT"
 
